9b3a65ee4df6d6d4bf8e94048f7519ee7c30a5b9
evg50 <f1.evgeniy4@gmail.com>
2025-09-26 16:21:15 +0200

    add user list

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,52 +1,61 @@
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Prisma } from '@prisma/client';
 
 import { Server, Socket } from 'socket.io';
 import { AppService } from 'src/app.service';
 
 @WebSocketGateway()
-export class AppGateway  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
-  constructor (
-    private appService: AppService, 
-    private users: Record<string, string> = {}
-
-  ){}
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private users: Record<string, string> = {};
+  constructor(private appService: AppService) {}
   @WebSocketServer() server: Server;
 
   @SubscribeMessage('sendMessage')
-  async handleSendMessage(client: Socket, payload: Prisma.ChatCreateInput): Promise<void> {
-    await this.appService.createMessage(payload)
-    this.server.emit('recMessage', payload)
+  async handleSendMessage(
+    client: Socket,
+    payload: Prisma.ChatCreateInput,
+  ): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
   }
 
-@SubscribeMessage('join')
-handleJoin(@MessageBody() username: string, @ConnectedSocket() client: Socket) {
-  this.users[client.id] = username;
+  @SubscribeMessage('join')
+  handleJoin(
+    @MessageBody() username: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.users[client.id] = username;
 
-  
-  this.server.emit('systemMessage', `${username} подключился`);
+    this.server.emit('systemMessage', `${username} подключился`);
 
-  
-  this.server.emit('usersList', Object.values(this.users));
-}
-
-
-
+    this.server.emit('usersList', Object.values(this.users));
+  }
 
   afterInit(server: any) {
-    console.log(server)
+    console.log(server);
   }
-  handleConnection (client: Socket) { 
+  handleConnection(client: Socket) {
     console.log(`Connected: ${client.id}`);
   }
 
- handleDisconnect(client: Socket) {
-  const username = this.users[client.id];
-  if (username) {
-    delete this.users[client.id];
+  handleDisconnect(client: Socket) {
+    const username = this.users[client.id];
+    if (username) {
+      delete this.users[client.id];
 
-    this.server.emit('systemMessage', `${username} отключился`);
-    this.server.emit('usersList', Object.values(this.users));
+      this.server.emit('systemMessage', `${username} отключился`);
+      this.server.emit('usersList', Object.values(this.users));
+    }
   }
 }
-
